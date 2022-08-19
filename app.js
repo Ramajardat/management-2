21aa3a511ddd9c4a0d3e6a88317e9c9dffdfa4f9
Ramajardat <ramajardat6@gmail.com>
2022-08-18 19:51:42 -0800

    add table

--- app.js
+++ app.js
@@ -1,163 +1,94 @@
-let allEmployees = [];
-
-const employee = function (employeeID, fullName, department, level, imageUrl) {
+function Employee(employeeID, fullName, department, level, imageURL, salary) {
   this.employeeID = employeeID;
   this.fullName = fullName;
   this.department = department;
   this.level = level;
-  this.imageUrl = imageUrl;
-  this.salary = salary(level);
-  function salary(level) {
-    switch (level) {
-      case "Senior":
-        return getRandomInt(1500, 2000);
-      case "Mid-Senior":
-        return getRandomInt(1000, 1500);
-      case "Junior":
-        return getRandomInt(500, 1000);
-    }
-  }
+  this.imageURL = imageURL;
+  this.salary = salaryCalc(level);
+}
+function salaryCalc(level) {
+  switch (level) {
+    case "Junior":
+      return Math.floor(Math.random() * 500) + 500;
 
-  function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+    case "Mid-Senior":
+      return Math.floor(Math.random() * 500) + 1000;
+    case "Senior":
+      return Math.floor(Math.random() * 500) + 1500;
   }
-};
-let employeeArray = [
-  new employee("1000", "Ghazi Samer", "Administration", "Senior", "google.com"),
-  new employee("1001", "Lana Ali", "Finance", "Senior", "google.com"),
-  new employee("1002", "Tamara Ayoube", "Marketing", "Senior", "google.com"),
-  new employee(
-    "1003",
-    "Safi Walid",
-    "Administration",
-    "Mid-Senior",
-    "google.com"
-  ),
-  new employee("1004", "Omar Zaid", "Development", "Senior", "google.com"),
-  new employee("1005", "Rana Saleh", "Development", "Junior", "google.com"),
-  new employee("1006", "Hadi Ahmad", "Finance", "Mid-Senior", "google.com"),
+}
+
+let person01 = new Employee(1000, "Gazi Sameer", "Advertisement", "Senior");
+let person02 = new Employee(1001, "Lana Ali", "Finance", "Senior");
+let person03 = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior");
+let person04 = new Employee(1003, "Safi Waleed", "Advertisement", "Mid-Senior");
+let person05 = new Employee(1004, "Omar Zaid", "Development", "Senior");
+let person06 = new Employee(1005, "Rana Saleh", "Development", "Junior");
+let person07 = new Employee(1006, "Hadi Ahmed", "Finance", "Mid-Senior", "");
+
+const table = document.createElement("table");
+const thead = document.createElement("thead");
+const tbody = document.createElement("tbody");
+
+let employees = [
+  person01,
+  person02,
+  person03,
+  person04,
+  person05,
+  person06,
+  person07,
 ];
+let static = ["Employee ID", "Full Name", "Department", "Level", "Salary"];
 
-let table = document.createElement("table");
-let tbody = document.createElement("tbody");
-for (const employee of employeeArray) {
-  console.log(" Name:" + employee.fullName + " Salary:" + employee.salary);
+function theadGenerator() {
+  const row = document.createElement("tr");
 
-  // createRow(employee);
+  for (let x = 0; x < 5; x++) {
+    const column = document.createElement("th");
+    const Content = document.createTextNode(static[x]);
+    column.appendChild(Content);
+    row.appendChild(column);
+  }
+
+  thead.appendChild(row);
+  table.appendChild(thead);
 }
 
-//TODO: make rowFunction
-//TODO: make colFunction
-//TODO: make tableFunction
+function tbodyGenerator() {
+  // create a rows
+  for (let x = 0; x < 7; x++) {
+    const row = document.createElement("tr");
+    // create a column inside the rows
+    for (let j = 0; j < 6; j++) {
+      const column = document.createElement("td");
+      if (j == 4) {
+        continue;
+      }
+      let person = Object.values(employees[x]);
+      const Content = document.createTextNode(person[j]);
 
-// function crateThead(employee) {
-//     let thead = document.createElement("thead");
-//     let tr = document.createElement("tr");
+      column.appendChild(Content);
+      row.appendChild(column);
+    }
+    tbody.appendChild(row);
+  }
 
-//     for (const iterator of Object.entries(employee)) {
-//         if (iterator[0] != "imageUrl") {
-//             let th = document.createElement("th");
-//             th.textContent = iterator[0];
-//             tr.append(th);
-//         }
-//     }
-//     thead.append(tr);
-
-//     return thead;
-// }
-
-// function createRow(employee) {
-//     let tr = document.createElement("tr");
-//     for (const iterator of Object.entries(employee)) {
-//         if (iterator[0] != "imageUrl") {
-//             let th = document.createElement("th");
-//             th.textContent = iterator[1];
-//             tr.append(th);
-//         }
-//     }
-
-//     tbody.append(tr);
-// }
-
-// table.append(crateThead(employeeArray[0]));
-// table.append(tbody);
-
-const main = document.getElementsByTagName("main");
-// main[0].append(table);
-
-let form = document.getElementById("form");
-let cardDiv = document.getElementById("cardAdd");
-
-function getData(event) {
-  let id = document.getElementById("ID").value;
-  let fullName = document.getElementById("fullName").value;
-  let department = document.getElementById("department").value;
-  let level = document.getElementById("level").value;
-  let imageUrl = document.getElementById("imageUrl").value;
-  let employeeForm = new employee(id, fullName, department, level, imageUrl);
-  console.log(employeeForm);
-  createCard(employeeForm);
-  event.preventDefault();
+  table.appendChild(tbody);
 }
 
-form.addEventListener("submit", getData);
-
-function createCard(employeeForm) {
-  let divCol = document.createElement("div");
-  divCol.className = "col-sm-6";
-  cardDiv.append(divCol);
-
-  let card = document.createElement("div");
-  card.className = "card text-dark bg-info mb-3";
-  divCol.append(card);
-
-  let imgCard = document.createElement("img");
-  imgCard.className = "card-img-top card text-dark bg-info mb-3";
-  imgCard.style.width = "100%";
-
-  imgCard.src = employeeForm.imageUrl;
-  card.append(imgCard);
-
-  let cardBody = document.createElement("div");
-  cardBody.className = "card-body";
-  card.append(cardBody);
-
-  let cardTitle = document.createElement("h5");
-  cardTitle.className = "card-title";
-  cardTitle.textContent = "Employee";
-  cardBody.append(cardTitle);
-
-  let cardText = document.createElement("p");
-  cardText.className = "card-text";
-  cardText.textContent =
-    "Name: " +
-    employeeForm.fullName +
-    " - ID: " +
-    employeeForm.employeeID +
-    " Department: " +
-    employeeForm.department +
-    " - Level: " +
-    employeeForm.level;
-  cardBody.append(cardText);
+function render() {
+  theadGenerator();
+  tbodyGenerator();
+  document.getElementById("main").appendChild(table);
+  for (let i = 0; i <= 6; i++) {
+    console.log(
+      "Employee Name: " +
+        employees[i].fullName +
+        ", Salary: " +
+        employees[i].salary
+    );
+  }
 }
 
-// function saveToLocal() {
-//   let strArr = JSON.stringify(allEmployees);
-//   localStorage.setItem("employees", strArr);
-// }
-
-// function getFromLocal() {
-//   let jsonArr = localStorage.getItem("employees");
-//   let arr = JSON.parse(jsonArr);
-//   allEmployees = arr;
-//   arr.forEach((ele) => {
-//     print(ele);
-//   });
-//   console.log(arr);
-// }
-
-// getFromLocal();
-
-// form.addEventListener("submit", render);
+render();
